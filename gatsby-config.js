e93323af1226efe569e96759efeef658aa54ced7
Bianca Cheng Costanzo <bianca@weave.works>
2022-08-06 07:56:57 +0200

    site config and about

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-emma-core/gatsby-config.js
-    siteTitle: `Emma`,
-    siteTitleAlt: `Emma - Gatsby Starter Portfolio`,
-    siteHeadline: `Emma - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://emma.lekoarts.de`,
-    siteDescription: `Minimalistic portfolio with full-width grid, page transitions, support for additional MDX pages, and a focus on large images`,
+    siteTitle: `De Volta`,
+    siteTitleAlt: `De Volta`,
+    siteHeadline: `De Volta is Gustavo Aguado, Bianca Cheng, Fabián Galico, and Adriano Valenzuela.`,
+    siteUrl: `https://bia.github.io/`,
+    siteDescription: `De Volta is Gustavo Aguado, Bianca Cheng, Fabián Galico, and Adriano Valenzuela.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@bia`,
   },
   plugins: [
     {
