30ab1d357fbe6f1f4fc49c1de6532886f42079e4
Bianca Cheng Costanzo <bianca@weave.works>
2022-08-06 16:53:25 +0200

    Updates

--- component---src-pages-404-jsx-912a432cce2f51147e41.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkemma=self.webpackChunkemma||[]).push([[256],{7291:function(e,t,n){n.r(t);var a=n(7294),l=n(1597),u=n(744),o=n(2877),r=n(3954),c=n(3444);t.default=function(){return a.createElement(r.Z,null,a.createElement(c.Z,{title:"404 - Not found"}),a.createElement(u.W2,null,a.createElement(o.Ge.p,null,"Oh, no!",a.createElement("br",null),"You found a page that doesn't exist."," ",a.createElement(u.rU,{as:l.Link,to:"/"},"Do you want to visit the homepage?"))))}}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-912a432cce2f51147e41.js.map
